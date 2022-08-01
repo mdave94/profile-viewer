3e42a0adde0dcde0c840343422cb2ed734421e1f
mdave94 <molnardavid2002@gmail.com>
2022-08-01 13:26:09 +0200

    Footer and pageses added

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+    return ( 
+   
+       <div>
+            <h1 className="text-6xl">ABOUT</h1>
+            
+        </div>
+     );
+}
+ 
+export default About;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import {FaHome} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+const NotFound = () => {
+    return ( 
+        <div className='hero'>
+            <div className="text-center hero-content">
+                <div className="max-w-lg">
+                    <h1 className="text-8xl font-bold mb-8">
+                        Whoops
+                    </h1>
+                    <p className="text-3xl mb-8">
+                        404- Page not found!
+                    </p>
+                    <Link className='btn btn-accent btn-lg' to="/">
+                        <FaHome className='mr-2'/>Back to HOME
+                    </Link>
+                </div>
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default NotFound;
