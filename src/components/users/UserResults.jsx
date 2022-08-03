ba0ea90d77a22197db78b45b9a9876d9123eea69
mdave94 <molnardavid2002@gmail.com>
2022-08-03 11:23:36 +0200

    userItem.jsx added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import Spinner from "../layouts/Spinner";
+import { useEffect, useState } from "react"
+import Spinner from "../layouts/Spinner"
+import UserItem from "./UserItem"
 
 
 
@@ -31,7 +32,7 @@ const UserResults = () => {
         return ( 
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user)=> (
-                    <h3 key={user.id}>{user.login}</h3>
+                    <UserItem key={user.id} user = {user}/>
                 ))}
             </div>
          );
